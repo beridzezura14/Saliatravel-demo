a5825a3caa91b62d5ce352011b49df9d63cb7fec
beridzezura14 <zuraberidze12@gmail.com>
2025-10-10 13:26:46 +0400

    add address

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // შენს backend URL
+  baseURL: 'hhttps://saliatravel-demo.onrender.com', // შენს backend URL
 });
 
 
